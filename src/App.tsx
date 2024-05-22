d14ac95b9a5d528072062a6a8d7d77ea7acdbdad
rafaellore <birdneon3@gmail.com>
2024-05-22 11:18:40 -0300

    feat(components): adds initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './Style.css'
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Resume from './pages/Resume';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+      <main>
+        <Header />
+        <Resume />
+      </main>
+    </div>
+  );
 }
 
 export default App
