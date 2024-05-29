4fa9e66d8476a436da2de506b6b53b1bc9575359
rafaellore <birdneon3@gmail.com>
2024-05-29 12:19:06 -0300

    feat(App): adds router-dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Style.css'
 import Header from './components/Header';
 import Sales from './components/Sales';
 import Sidenav from './components/Sidebar';
 import { DataContextProvider } from './contexts/DataContext';
 import Resume from './pages/Resume';
+import Sell from './components/Sell';
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resume />
-          <Sales />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path='/' element={<Resume />} />
+              <Route path='/vendas' element={<Sales />} />
+              <Route path='/vendas/:id' element={<Sell />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider></BrowserRouter>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import DateRange from "./DateRange";
 import Months from "./Months";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
   const [title, setTitle] = React.useState("Resumo");
+  const location = useLocation();
+  React.useEffect(() => {
+    if (location.pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else if (location.pathname === "/vendas") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [location]);
 
   return (
     <header className="mb">
